7305274637ea761e7a4ff39d1e2d0aa31e3ac672
Diego Alves Teixeira <diegoteialves@gmail.com>
2024-03-11 15:29:40 -0300

    Utilizando banco de dados fake, criei metodo get por id, get geral e put para o usuario

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const app = express();
 
 app.use(express.json());
 
+const users = []
 
 app.get('/alunos', (req, res) => {
     console.log(req.query)
     console.log('entrou alunos'); 
-    res.status(200).send('Deu certo');
+    res.status(200).send({users: users});
 })
 
 app.get('/alunos/:id', (req, res) => {
-    console.log(req.params.id)
-    console.log('entrou alunos'); 
-    res.status(200).send('Deu certo');
+    const alunoId = req.params.id
+    
+    res.status(200).send(users.find(x => x.id == alunoId));
 })
 
 app.post('/alunos', (req, res) =>{
     console.log(req.body)
+    users.push(req.body)
     console.log('post alunos')
-    res.status(200).send('Deu certo')
+    res.status(200).send(req.body)
 })
 
 app.put('/alunos/:id', (req, res) =>{
